7a493c9fab4f070a37b649d47241471dd5700840
hndrisywvi <hendrisyuwavi12@gmail.com>
2023-11-28 21:41:11 +0700

    test

--- app/main.py
+++ app/main.py
@@ -6,10 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["GET"]) #standart routing
 def login():
     data = request.get_json()
     return jsonify(data)
+
+@app.route("/path-variable/<name>", methods=["GET"]) #path variable
+def path_variable(name):
+    strWord = f"nama saya: {0}".format(name)
+    return jsonify(strWord)
 
 if __name__=="__main__":
     app.run(debug=True)
